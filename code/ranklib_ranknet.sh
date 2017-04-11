70bc0a576e70e89c48a6d348e454bee8891bb688
phillipmortimer <phillip@phillipmortimer.com>
2017-04-12 00:45:19 +0100

    RankLib RankNet shell full update.  Run different param setting b2b and save model file.

--- code/ranklib_ranknet.sh
+++ code/ranklib_ranknet.sh
@@ -1,1 +1,8 @@
-java -jar ../RankLib/RankLib.jar -train ../data/Fold1/train.txt -test ../data/Fold1/test.txt -ranker 1 -epoch 1 -layer 1 -node 10 -lr 0.00005 -save ../model/ranknet1_model.txt
+now=$(date +"%T")
+echo "Current time : $now"
+java -jar ../RankLib/RankLib.jar -train ../data/Fold1/train.txt -test ../data/Fold1/test.txt -ranker 1 -norm linear -epoch 3 -layer 1 -node 10 -lr 0.00001 -save ../model/rn_f1_e3_l1_n10_lr-5.txt
+now=$(date +"%T")
+echo "Current time : $now"
+java -jar ../RankLib/RankLib.jar -train ../data/Fold1/train.txt -test ../data/Fold1/test.txt -ranker 1 -norm linear -epoch 3 -layer 1 -node 10 -lr 0.0001 -save ../model/rn_f1_e3_l1_n10_lr-4.txt
+now=$(date +"%T")
+echo "Current time : $now"
